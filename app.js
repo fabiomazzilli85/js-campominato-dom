7e4265c66b5735e35b1a9e196918e8835dd9d1c3
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-03-14 12:48:05 +0100

    Riscrittura del codice. Numeri randomici in Array OK. In console.log, testo OK.

--- app.js
+++ app.js
@@ -2,47 +2,57 @@ document.addEventListener("DOMContentLoaded", function() {
     const container = document.querySelector('.container'); 
     const showButton = document.getElementById('showButton');
     
+    // Array dei numeri bomba.
+    // 1. Creo una costante e le do il nome di bombNumber.
+    // 2. Con il ciclo For incremento di uno la variabile ''i'' fino a che è vera la condizione che ''i'' <= di 16.
+    // 3. Con includes mi accerto che un numero non si ripeta.
+    const bombNumbers = [];
+    for (let i = 1 i <= 16; i++) {
+        let bombNumber;
+        do {
+            bombNumber = Math.floor(Math.random() * 100) + 1;
+        } while (bombNumbers.includes(bombNumber)); 
+        bombNumbers.push(bombNumber);
+    }
 
 
+    // Creo un ciclo For.
+    // 1. Creo una variabile che, partendo da 1, si incrementa di 1 fino a che è vera la condizione che la variabile è  <=100.
+    // 2. Aggiungo due classi: box e hidden.
+    // 3. Creo il contenuto = i.
+    // 4. Al listener '''gioco'' con la rimozione e l'aggiunta delle classi.  
     for (let i = 1; i <= 100; i++) {
-        const div = document.createElement('div');
-        div.classList.add('box', 'hidden');
-        div.textContent = i; 
+        const div = document.createElement('div'); 
+        div.classList.add('box', 'hidden'); 
+        div.textContent = i;  
         div.addEventListener('click', function() {
-            
             document.querySelectorAll('.box').forEach(box => {
                 box.classList.remove('clicked'); 
             });
             
+
+    // // Questa è la parte più importante fino a questo momento. Se il bombNumber è incluso (tramite il metodo includes) nei clickedNumber mando a schermo il messaggio in console.log con la logica dell'if ed else
             this.classList.add('clicked'); 
             const clickedNumber = parseInt(this.textContent);
-            console.log("Hai cliccato sul div numero:", clickedNumber);
+            if (bombNumbers.includes(clickedNumber)) { 
+                console.log("Hai cliccato su una bomba. Mi dispiace");
+            } else {
+                console.log("Ti è andata bene...");
+            }
         });
         container.appendChild(div);
     }
     
-    showButton.addEventListener('click', function() {
-        
-        
-        const boxes = document.querySelectorAll('.box');
-        
     
+    // Qui lavoro sul bottone.
+    // 1. Quando clicco sul bottone, esso rimuove la classe hidden dal mio box che, di conseguenza, risulta visualizzabile (la classe hidden, nel file CSS, ha proprietà Display None). 
+    // 2. Riga 55: dopo aver cliccato sul bottone, lavoro con lo style e lo mando in display none. 
+    showButton.addEventListener('click', function() {
+        const boxes = document.querySelectorAll('.box');
         boxes.forEach(box => {
             box.classList.remove('hidden');
         });
         
-            showButton.style.display = 'none';
+        showButton.style.display = 'none'; 
     });
 });
-
-
-// Ho creato una costante e ho inserito manualmente 16 numeri e ho fatto un console.log.. Ora farò in modo che in numeri siano generati casualmente e commenterò il mio Array. 
-const bombNumber = []
-
-for (let i = 1; i <= 16; i++) {
-    bombNumber.push(Math.floor(Math.random() * 100) + 1);
- }
-
- console.log(bombNumber.length);
-
- // Utilizzando il ciclo Fior ho appena generato un numero compreso tra 1 e 16 e ho eseguito il ''push'' in ''bombNumber', dichiarato precedentemente. '
